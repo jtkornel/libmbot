2a03f111cc2f57c28e015ca5e8db3950dec46b62
Jan Tore Korneliussen <jtkornel@yahoo.no>
2023-09-03 23:43:15 +0200

    auriga example, more set-operations

--- examples/auriga_board.cpp
+++ examples/auriga_board.cpp
@@ -1,5 +1,6 @@
 #include "boards.hpp"
 
+
 using namespace libmbot;
 
 int main()
@@ -10,9 +11,27 @@ int main()
 
     AurigaBoard b(c);
 
-    b.m_motor_1.set_speed_motion.request(100);
+    std::cout << "Setting RGB LEDs to white\n";
+    for(uint8_t li=0; li < 12; li++) {
+        RGBLedColor white(li+1, RGBColor {100, 100, 100});
+        b.m_rgb_led.set_color.request(white);
+        usleep(100000);
+    }
 
-    usleep(1000000);
+    PosMotion pm1 {400, 40};
+    PosMotion pm2 {-400, 40};
+    std::cout << "Positioning motors\n";
+    b.m_motor_1.set_pos_motion.request(pm1);
+    b.m_motor_2.set_pos_motion.request(pm2);
 
+    usleep(4000000);
+
+    std::cout << "Stopping motors\n";
     b.m_motor_1.set_speed_motion.request(0);
+    b.m_motor_2.set_speed_motion.request(0);
+
+
+    std::cout << "Setting RGB LEDs to black\n";
+    RGBLedColor all_black(0, RGBColor {0, 0, 0});
+    b.m_rgb_led.set_color.request(all_black);
 }
